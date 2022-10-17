da34524f202fa76f6a1a3e1aa334db3116ff1a73
Matheus França <75399028+matheusft20@users.noreply.github.com>
2022-10-17 09:46:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,400 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>My Gesthor </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div id="logo">
+        <h1><a href="index.php">My Gesthor</a></h1>
+
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#call-to-action">Inscreva-se</a></li>
+          <li><a class="nav-link scrollto" href="#about">Sobre nós</a></li>
+          <li><a class="nav-link scrollto" href="#more-features">Conteúdo</a></li>
+
+          
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+
+
+
+
+
+
+  <div class="hero-text" data-aos="zoom-out">
+      <h2>Seja bem vindo a My Gesthor </h2>
+      <p>Inscreva-se agora e se torne um Gestor Hospitalar</p>
+      <a href="#call-to-action" class="btn-get-started scrollto">Inscreva-se</a>
+    </div>
+
+  <!-- 
+    <div class="content-login">
+      <h2 class="active"> Inscrição </h2>
+      <form class="box-login" method="post" action="#">
+        <input type="email" id="email" class="campo" name="email" placeholder="E-mail">
+        <input type="text" id="password" class="campo" name="senha" placeholder="Senha">
+        <input type="telefone" id="telefone" class="campo" name="telefone" placeholder="(DDD) 99999-9999">
+        <input type="submit" class="botao" name="enviar" value="Inscrever">
+      </form>
+</div>
+
+
+-->
+
+  </section><!-- End Hero Section -->
+
+
+
+
+
+
+
+  <main id="main">
+
+
+
+
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="section-bg">
+      <div class="container-fluid" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title">QUEM SOMOS?</h3>
+          <span class="section-divider"></span>
+          <p class="section-description">
+
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 about-img" data-aos="fade-right" dat-aos-delay="100">
+            <img src="assets\img\rauleelias.png">
+          </div>
+
+          <div class="col-lg-6 content" data-aos="fade-left" dat-aos-delay="100">
+            <h2>A Gesthor surgiu em 1998. Implantando em um único sistema (softwares) a solução para a automatizar todo o processo hospitalar, reduzindo custos e trazendo agilidade no atendimento aos pacientes e em todo o processo de gestão. Nós aprimoramos nossa expertise em desenvolvimento de Soluções de Alta Performance em Tecnologia direcionadas para a Gestão completa de Instituições de Saúde.</h2>
+            <h3></h3>
+            <p>
+            Com esse trabalho, treinamos pessoas para todas as áreas, gerindo-as, desde então. E vamos ampliar essa gama, proporcionando inúmeras oportunidades para mais pessoas descobrirem o ilimitado campo da saúde.
+            </p>
+
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Desenvolver uma visão panorâmica de uma instituição de saúde.</li>
+              <li><i class="bi bi-check-circle"></i> Tomar decisões mais assertivas.</li>
+              <li><i class="bi bi-check-circle"></i> Desenvolver uma visão panorâmica de uma instituição de saude</li>
+            </ul>
+
+            <p>
+            Para gestores que querem dar um up no seu time e dar visão mais abrangente da área da saúde. Vaga individual
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Featuress Section ======= -->
+    
+    
+    <!-- ======= Advanced Featuress Section ======= -->
+    <section id="advanced-features">
+
+      <div class="features-row section-bg" data-aos="fade-up">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <img class="advanced-feature-img-right wow fadeInRight" src="assets\img\imagem - 3 .png" alt="">
+              <div>
+                <h2>COMO SURGIU A IDEIA DO PROJETO?</h2>
+                <p>Este curso foi desenhado levando em consideração as maiores dificudades encontradas em centenas de empresas onde prestamos consultorias e implantação de processos e implantação de ERP. A mesma reclamação dos gestores e donos de clínicas e hospitais.</p>
+                <p>As empresas têm dificuldade de encontrar profissionais qualificados, e mesmo os que possuem alguma formação, falta competência dehabilidades que não aprendemos com livros.</p>
+                <p>Nossa experiência pode sanar esse problema, atuamos treinando pessoas nos diversos segmentosda saúde.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="features-row" data-aos="fade-up">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <img class="advanced-feature-img-left" src="assets\img\imagem - 2 .png" alt="">
+              <div>
+                <h2>PRA QUEM É ESSE CURSO?</h2>
+                <p>-Para quem quer o conhecimento básico para ingressar na área da saúde e como aplicar esse conhecimento dentro da ocupação certa na área que nunca vai perder espaço e sempre vai precisar de mais gente qualificada.</p>
+                <p>- Para quem quer entender os principais conceitos e como aplicá-los no dia a dia no seu primeiro emprego ou mudar de área.</p>
+                <p>Para quem quer saber como melhorar seus resultados, ter melhores ganhos, ser mais pró-ativo e eficiente</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     
+    </section><!-- End Advanced Featuress Section -->
+
+
+
+    <!-- ======= More Features Section ======= -->
+    <section id="more-features" class="section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h3 class="section-title">CONTEÚDO</h3>
+          <span class="section-divider"></span>
+         
+        </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-6">
+            <div class="box">
+              <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Por que a área da saúde se tornou tão promissora.</a></h4>             
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Recrutamento na área da saúde.</a></h4>         
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Depoimentos de especialistas que tiveram sucesso no seu planejamento de carreira na área da saude</a></h4>             
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Seja um profissional exemplar no comportamento, currículo, entrevista de emprego e quais habilidades adquirir para isso!</a></h4>              
+            </div>
+          </div>
+          
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução Recepção Hospitalar</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução Centro Cirúrgico</a></h4> 
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução Faturamento Hospitalar</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução em Compras Hospitalar</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução em Almoxarifado/Farmacia-Hospitalar</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">Introdução em Financeiro Hospitalar</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">BÔNUS DE LIVES COM EXPERTS DE TODAS ESSAS ÁREAS.</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="box">
+            <div class="icon"><i class="bi bi-book"></i></div>
+              <h4 class="title"><a href="">BÔNUS DE LIVES COM EXPERTS DE TODAS ESSAS
+ÁREAS.
+</a></h4>              
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </section><!-- End More Features Section -->
+
+
+      <!-- ======= Call To Action Section ======= -->
+
+      <section id="call-to-action" class="form-call">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-start">
+            <h3 class="cta-title">Inscrição</h3>
+            <p class="cta-text"></p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+
+          </div>
+        </div>
+        <div class="inscricao">
+        <form action="./inscricao.php" method="post" class="form-insc">  
+          <h3 class="cta-title">De 10 de outubro a 01 de dezembro</h3>
+          <input type="hidden" name="nome" class="input-form">
+          <input type="text" placeholder="Nome" id="nome" name="nome" value=""class="input-form">
+          <input type="email" placeholder="E-mail" id="email" name="email" value=""class="input-form">
+          <input type="tel" placeholder="(DDD) 99999-9999" id="telefone" name="telefone" value=""class="input-form">
+          <div class="col-lg-3 cta-btn-container text-center">
+            <input type="submit"value="Inscrever" id="submit" name="Enviar" class="input-enviar">
+          </div>
+                
+            </form>
+        </div>
+      </div>
+
+
+      <div class="wrapper fadeInDown">
+  
+
+
+  </div><!--Form Content-->
+</div><!--wrapper-->
+
+
+
+    </section><!-- End Call To Action Section -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-4">
+            <div class="contact-about">
+              <h3>My Gesthor</h3>
+              <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+              <div class="social-links">
+                <a href="https://instagram.com/gesthorhospitalar?igshid=YmMyMTA2M2Y=" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/gesthor-hospitalar-68b29a16a" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                <a href="#"><i class="bi bi-geo-alt"></i></a>
+                <a href="#"><i class="bi bi-envelope"></i></a>
+                <a href="#"><i class="bi bi-phone"></i></a>
+
+              </div>
+            </div>
+          </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 text-lg-start text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>My Gesthor</strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+
+            Designed by <a href="#/">DIEX</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-chevron-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
